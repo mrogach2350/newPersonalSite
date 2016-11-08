b237a34ba287c8e600b7b2adc896af1138082a27
mrogach2350 <m.rogach@gmail.com>
2016-11-08 10:55:24 -0800

    most recent changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-$(#tweeter).on('click', function handleEvent(event){
-  event.preventDefault();
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    $('.collapsible').collapsible();
+    $( '.fa-shield' ).on('hover', function(){
+      console.log('hovering...');
+    });
+
+
 });
